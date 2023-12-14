ca96a8449a975cc8823d989edd7a398bbbf2ee2f
AhmetinzmSec <picadroattack@outlook.com>
2023-12-15 02:17:55 +0300

    Noel Update Part 1

---
+++ assets/js/pop-up.js
@@ -0,0 +1,10 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+});
+
+function closePopup() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'none';
+}
