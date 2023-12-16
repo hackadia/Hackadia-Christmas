af42472b4a0f78a8c95d71428fdf2546a4b49550
AhmetinzmSec <picadroattack@outlook.com>
2023-12-16 12:26:33 +0300

    Footer and Notification Update

--- assets/js/pop-up.js
+++ assets/js/pop-up.js
@@ -8,3 +8,8 @@ function closePopup() {
     const popup = document.getElementById('popup');
     popup.style.display = 'none';
 }
+
+function openNotification() {
+    const popup = document.getElementById('popup');
+    popup.style.display = 'flex';
+}
